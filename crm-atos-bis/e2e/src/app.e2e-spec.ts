f576ef23be373771a9dad040461152cf8c9356ab
nabil <nabil.attari6@gmail.com>
2020-03-02 14:53:48 +0100

    crm-atos-bis
    second commit

--- crm-atos-bis/e2e/src/app.e2e-spec.ts
+++ crm-atos-bis/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('crm-atos app is running!');
+    expect(page.getTitleText()).toEqual('crm-atos-bis app is running!');
   });
 
   afterEach(async () => {
