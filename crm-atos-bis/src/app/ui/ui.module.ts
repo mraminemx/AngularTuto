9e9ed7ddbb26ae57f53b9f4fb78891c603c4fdeb
nabil <nabil.attari6@gmail.com>
2020-03-03 10:22:33 +0100

    selecteurs
    balise ng-content and the use of select attribut and html component or css class ou the id of the attributes

--- crm-atos-bis/src/app/ui/ui.module.ts
+++ crm-atos-bis/src/app/ui/ui.module.ts
@@ -9,7 +9,7 @@ import { UiComponent } from './components/ui/ui.component';
 
 @NgModule({
   declarations: [UiComponent, HeaderComponent, NavComponent, FooterComponent],
-  exports: [UiComponent],
+  exports: [UiComponent,NavComponent,HeaderComponent,FooterComponent],
   imports: [
     CommonModule
   ]
