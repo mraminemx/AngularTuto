a83a04782fe1bb1d5af51eab78ca8c6949f92b80
nabil <nabil.attari6@gmail.com>
2020-03-03 14:11:53 +0100

    using css properties
    using css properties of /stylings directory

--- crm-atos-bis/src/app/ui/components/header/header.component.ts
+++ crm-atos-bis/src/app/ui/components/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public title = 'Nabil\'s App';
   constructor() { }
 
   ngOnInit(): void {
