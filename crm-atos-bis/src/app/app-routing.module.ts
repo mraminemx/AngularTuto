7073de63f7a2a896dc7429579d83849783d77365
nabil <nabil.attari6@gmail.com>
2020-03-03 15:21:48 +0100

    Lifecycles / Properties-binding /Routing
    Everytime ajax call with component.
    *Lifecycles hooks are called when there is an interactions.
    Angular call mechanism called ngOnChanges() detection to know what html view need to be updated in a lifecycles [https://angular.io/guide/lifecycle-hooks]
    
    *Properties-binding we have to add Decorator @Input()
    
    *The Angular Router is not part of Angular Core "library package, @angular/router" [https://angular.io/guide/router]
    
    Commentaire : composant parent (Logic) pour traiter la données  et des composants basic (design) pour afficher les datas

--- crm-atos-bis/src/app/app-routing.module.ts
+++ crm-atos-bis/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PageLoginComponent} from './login/pages/page-login/page-login.component';
 
+//router is a singleton can be initialized only on time
 
-const routes: Routes = [];
+const routes: Routes = [
+//  { path: 'crisis-center', component: CrisisListComponent },
+ // { path: 'hero/:id',      component: HeroDetailComponent },
+  {
+    path: 'login',
+    component: PageLoginComponent//Composant/Components Login
+  },
+  { path: '',
+    redirectTo: '/login',// url/path if  redirectTo: '/login' -> url/login. if redirectTo: 'login' -> url/path/login
+    pathMatch: 'full'
+  }
+  //,{ path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  //enableTracing
+  imports: [RouterModule.forRoot(routes, {enableTracing:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- crm-atos-bis/src/app/login/login.module.ts
+++ crm-atos-bis/src/app/login/login.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageLoginComponentsComponent } from './pages/page-login-components/page-login-components.component';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PageLoginComponentsComponent, PageLoginComponent],
   imports: [
     CommonModule
   ]
