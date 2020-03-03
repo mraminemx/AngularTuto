e6bc99c3cccc2cf63ba60173d19fd93c5265d519
nabil <nabil.attari6@gmail.com>
2020-03-03 15:49:39 +0100

    Creation des modules
    Modules and Pages

--- crm-atos-bis/src/app/app-routing.module.ts
+++ crm-atos-bis/src/app/app-routing.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {PageLoginComponent} from './login/pages/page-login/page-login.component';
+import { PageNotFoundComponent } from './page-not-found/pages/page-not-found/page-not-found.component';
+import { PageClientsComponent } from './clients-module/pages/page-clients/page-clients.component';
+import { PagePrestationsComponent } from './prestations-module/pages/page-prestations/page-prestations.component';
 
 //router is a singleton can be initialized only on time
 
 const routes: Routes = [
-//  { path: 'crisis-center', component: CrisisListComponent },
- // { path: 'hero/:id',      component: HeroDetailComponent },
+ { path: 'clients', component: PageClientsComponent },
+ { path: 'prestations',      component: PagePrestationsComponent },
   {
     path: 'login',
-    component: PageLoginComponent//Composant/Components Login
-  },
+    component: PageLoginComponent,
+    data: { title: 'Heroes List' }
+   },
   { path: '',
     redirectTo: '/login',// url/path if  redirectTo: '/login' -> url/login. if redirectTo: 'login' -> url/path/login
     pathMatch: 'full'
-  }
-  //,{ path: '**', component: PageNotFoundComponent }
+  },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
