cf60ceb8afe97e68ed716a3de3fc15ad008a243a
nabil <nabil.attari6@gmail.com>
2020-03-04 17:23:32 +0100

    Host Binding to access td property
    Binding of directive attribute to a property in html elment example 'td'

--- crm-atos-bis/src/app/shared-module/directives/state.directive.ts
+++ crm-atos-bis/src/app/shared-module/directives/state.directive.ts
@@ -1,14 +1,16 @@
-import { Directive, Input, OnInit } from '@angular/core';
+import { Directive, Input, OnInit, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appState]'
 })
 export class StateDirective implements OnInit{
+  @Input() appState:any;
+  //Host Binding to access td property
+  @HostBinding('class') classTd:string;
   ngOnInit(): void {
     console.log(this.appState);
   }
 
-  @Input() appState:any;
   constructor() {
   }
 
