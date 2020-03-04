cb6420c35cca4401ae7aa03aaccba68cde7d0a34
nabil <nabil.attari6@gmail.com>
2020-03-04 10:43:40 +0100

    Add Url API and get collection
    Create  crud methods and add url Api

--- crm-atos-bis/src/app/prestations-module/services/prestations.service.ts
+++ crm-atos-bis/src/app/prestations-module/services/prestations.service.ts
@@ -1,22 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Prestation } from 'src/app/shared-module/models/prestation';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrestationsService {
 
-  constructor(private http: HttpClient) { }
+  //Observable to be able to subscribe to it
+  private pCollection:Observable<Prestation[]>;
+  constructor(private http: HttpClient) {
+    //httpCall this.http.get(this.configUrl);
+    this.collection =http.;
+  }
 
   // get collection
-
+  public get collection():Observable<Prestation[]> {
+    return this.pCollection;
+  }
   //set collection
-
+  public set collection(col:Observable<Prestation[]>) {
+    this.pCollection = col;
+  }
   // add item in collection
-
+  public addItem(item:Prestation){
+    this.pCollection;
+  }
   // delete item in collection
-
+  public deleteItem(item:Prestation){
+    this.pCollection;
+  }
   // update item in collection
-
+  public updateItem(item:Prestation) {
+    this.pCollection;
+  }
   // get item by id
+  public getItem(){
+  }
 }
