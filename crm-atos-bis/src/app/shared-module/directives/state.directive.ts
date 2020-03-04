d807e46548108812d51dd745b1659ab9888784d1
nabil <nabil.attari6@gmail.com>
2020-03-04 17:35:40 +0100

    OnChanges in state directive
    OnChanges implementation in state directive for every change

--- crm-atos-bis/src/app/shared-module/directives/state.directive.ts
+++ crm-atos-bis/src/app/shared-module/directives/state.directive.ts
@@ -1,15 +1,24 @@
-import { Directive, Input, OnInit, HostBinding } from '@angular/core';
+import { Directive, Input, OnInit, HostBinding, OnChanges } from '@angular/core';
 
 @Directive({
   selector: '[appState]'
 })
-export class StateDirective implements OnInit{
+export class StateDirective implements OnChanges{
+
   @Input() appState:any;
   //Host Binding to access td property
   @HostBinding('class') classTd:string;
-  ngOnInit(): void {
+  // ngOnInit(): void {
+  //   console.log(this.appState);
+  //   this.classTd =this.formatClass(this.appState);
+  // }
+  private formatClass(state: any): string {
+    return `state-${state.normalize("NFD").replace(/[\u0300-\u036f\s]/g, "").toLowerCase()}`;
+  }
+
+  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
     console.log(this.appState);
-    this.classTd ='state-confirme';
+    this.classTd =this.formatClass(this.appState);
   }
 
   constructor() {
