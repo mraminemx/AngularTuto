24e7c1360d3888b2accf73ad40139dd093764d8d
nabil <nabil.attari6@gmail.com>
2020-03-05 14:00:36 +0100

    Using Template A
    Using Template A

--- crm-atos-bis/src/app/templates/templates/template-a/template-a.component.ts
+++ crm-atos-bis/src/app/templates/templates/template-a/template-a.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-template-a',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateAComponent implements OnInit {
 
+  @Input() title:string;
+  @Input() subtitle:string;
   constructor() { }
 
   ngOnInit(): void {
--- crm-atos-bis/src/app/templates/templates/template-b/template-b.component.ts
+++ crm-atos-bis/src/app/templates/templates/template-b/template-b.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-template-b',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateBComponent implements OnInit {
 
+  @Input() title:string;
+  @Input() subtitle:string;
   constructor() { }
 
   ngOnInit(): void {
