26903bcae42efb444563f41c9cfa3311e4eec3e3
nabil <nabil.attari6@gmail.com>
2020-03-05 10:01:49 +0100

    component creation
    ng g component shared-module/components/tableau-light --export

--- crm-atos-bis/src/app/shared-module/shared-module.module.ts
+++ crm-atos-bis/src/app/shared-module/shared-module.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TotalPipe } from './pipes/total.pipe';
 import { StateDirective } from './directives/state.directive';
+import { TableauLightComponent } from './components/tableau-light/tableau-light.component';
 
 
 
 @NgModule({
-  declarations: [TotalPipe, StateDirective],
-  exports: [TotalPipe, StateDirective],
+  declarations: [TotalPipe, StateDirective, TableauLightComponent],
+  exports: [TotalPipe, StateDirective, TableauLightComponent],
   imports: [
     CommonModule
   ]
